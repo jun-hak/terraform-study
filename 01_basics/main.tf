efa4b32b232cff0f257938fb207320a8d096fd92
Jun <keea9@naver.com>
2025-10-18 23:04:42 +0900

    feat: add count example

--- 01_basics/main.tf
+++ 01_basics/main.tf
@@ -3,8 +3,9 @@
 
 resource "local_file" "tf_example1" {
 #   filename = "01_basics/example.txt"
-    filename = "${path.module}/example1.txt"
+    filename = "${path.module}/example${count.index}.txt"
     content = "updated content"
+    count = 3
 }
 
 resource "local_sensitive_file" "tf_example2" {
