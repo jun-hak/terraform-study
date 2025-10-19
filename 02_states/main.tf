cfa3cdfc1075e750e40687b3722c46a336d4a24f
Jun <keea9@naver.com>
2025-10-19 15:55:39 +0900

    feat: terraform state

---
+++ 02_states/main.tf
@@ -0,0 +1,16 @@
+
+resource "local_file" "example1" {
+#   filename = "01_basics/example.txt"
+    filename = "${path.module}/example1.txt"
+    content = "this is demo content - 1"
+}
+
+resource "local_file" "example2" {
+    filename = "${path.module}/example2.txt"
+    content = "this is demo content - 2"
+}
+
+resource "local_sensitive_file" "tf_sensitive" {
+    filename = "${path.module}/sensitive.md"
+    content = "kunal123!"
+}
